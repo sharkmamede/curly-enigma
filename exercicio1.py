659c40d810a088960829a098876d0d89eefb4b3e
sharkmamede <rui.mamede@sharkcoders.pt>
2024-10-12 20:18:32 +0100

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,6 @@
+numero1 = int(input("Por favor, introduza um número e prima Enter:"))
+numero2 = int(input("Coloque o número 2, por favor:"))
+
+media = (numero1 + numero2) / 2
+
+print("A média dos dois números introduzidos é", media)
